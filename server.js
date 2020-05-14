f0e649adef06eb675ddbf24bc98d9fd24098f1c6
palhamel <pal.hamel@gmail.com>
2020-05-14 14:46:29 +0200

    add mongodb setup

--- server.js
+++ server.js
@@ -1,6 +1,22 @@
 import express from 'express'
 import bodyParser from 'body-parser'
 import cors from 'cors'
+import mongoose, { MongooseDocument, Mongoose } from 'mongoose'
+
+// Database setup:
+const mongoUrl = process.env.MONGO_URL || "mongodb://localhost/mongo-validation-codealong";
+mongoose.connect(mongoUrl, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.Promise = Promise;
+
+// Mongoose model setup:
+
+const Person = mongoose.model('Person', {
+  
+})
+
+
+
+
 
 // Defines the port the app will run on. Defaults to 8080, but can be 
 // overridden when starting the server. For example:
@@ -15,7 +31,7 @@ app.use(bodyParser.json())
 
 // Start defining your routes here
 app.get('/', (req, res) => {
-  res.send('Hello world')
+  res.send('Hello man!')
 })
 
 // Start the server
